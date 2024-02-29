2116615e7de5e988684d66088241e92034653849
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2024-02-29 09:57:49 -0800

    feat: workflow changes for tagging and deploying releases

--- generator-nr-maven-build/generators/app/templates/build-intention.sh
+++ generator-nr-maven-build/generators/app/templates/build-intention.sh
@@ -7,5 +7,6 @@ cat ./.github/workflows/build-intention.json | jq "\
     (.actions[] | select(.id == \"build\") .service.project) |= \"${SERVICE_PROJECT}\" | \
     (.actions[] | select(.id == \"build\") .service.name) |= \"${SERVICE_NAME}\" | \
     (.actions[] | select(.id == \"build\") .package.version) |= \"${PROJECT_VERSION}\" | \
-    (.actions[] | select(.id == \"build\") .package.buildVersion) |= \"${GIT_COMMIT}\" \
+    (.actions[] | select(.id == \"build\") .package.buildVersion) |= \"${GIT_COMMIT}\" | \
+    (.actions[] | select(.id == \"build\") .package.buildNumber) |= ${BUILD_NUMBER} \
     " > intention.json

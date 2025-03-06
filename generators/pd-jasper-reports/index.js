542a5f68f6966a17715b446d64e98b292a318b49
Andreas Wilson <41663792+andrwils@users.noreply.github.com>
2025-03-06 10:14:27 -0800

    feat: jasper reports workflow and intention files (#99)

--- generators/pd-jasper-reports/index.js
+++ generators/pd-jasper-reports/index.js
@@ -7,7 +7,7 @@ import chalk from 'chalk';
 import * as fs from 'node:fs';
 
 /**
- * Generate the Ansible playbook needed for Jasper Reports deployments
+ * Generate the files needed for Jasper Reports deployments
  */
 export default class extends Generator {
   constructor(args, opts) {
@@ -18,10 +18,25 @@ export default class extends Generator {
       required: true,
       description: 'Project Name',
     });
+    this.argument('serviceName', {
+      type: String,
+      required: true,
+      description: 'Service Name',
+    });
+    this.argument('jasperProjectName', {
+      type: String,
+      required: true,
+      description: 'Jasper Project Name',
+    });
     this.argument('jasperServerInstance', {
       type: String,
       required: true,
       description: 'Jasper Server Instance',
+    });
+    this.argument('brokerJwt', {
+      type: String,
+      required: true,
+      description: 'Broker JWT',
     });
     this.argument('playbookPath', {
       type: String,
@@ -30,24 +45,34 @@ export default class extends Generator {
     });
   }
 
-  // Generate playbook file
+  // Generate files
   writing() {
-    this.log('Generating playbook file');
-    if (
-      !fs.existsSync(
-        this.destinationPath(`${this.options.playbookPath}/jasper-reports.yaml`),
-      )
-    ) {
-      this.fs.copyTpl(
-        this.templatePath('jasper-reports.yaml'),
-        this.destinationPath(`${this.options.playbookPath}/jasper-reports.yaml`),
-        {
-          projectNameUpperCase: this.options.projectName.toUpperCase(),
-          jasperServerInstanceUpperCase: this.options.jasperServerInstance.toUpperCase(),
-        },
-      );
-    }
-
+    this.log('Generating files');
+    this.fs.copyTpl(
+      this.templatePath('jasper-reports-workflow.yaml'),
+      this.destinationPath('.github/workflows/jasper-reports.yaml'),
+      {
+        projectName: this.options.projectName,
+        serviceName: this.options.serviceName,
+        brokerJwt: this.options.brokerJwt,
+      },
+    );
+    this.fs.copyTpl(
+      this.templatePath('jasper-reports-intention.json'),
+      this.destinationPath('.jenkins/jasper-reports-intention.json'),
+      {
+        projectName: this.options.projectName,
+        serviceName: this.options.serviceName,
+      },
+    );
+    this.fs.copyTpl(
+      this.templatePath('jasper-reports-playbook.yaml'),
+      this.destinationPath(`${this.options.playbookPath}/jasper-reports.yaml`),
+      {
+        projectNameUpperCase: this.options.jasperProjectName.toUpperCase(),
+        jasperServerInstanceUpperCase: this.options.jasperServerInstance.toUpperCase(),
+      },
+    );
     this.config.save();
   }
 }

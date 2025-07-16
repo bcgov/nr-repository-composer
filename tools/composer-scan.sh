65f3ff9d531f4960cff254974b2435290fbcc061
van der Hooft, David WLRS:EX <david.vanderhooft@gov.bc.ca>
2025-07-16 14:25:34 -0700

    feat: parameterize ORG

--- tools/composer-scan.sh
+++ tools/composer-scan.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-ORG="bcgov-c"
+# Check if the required arguments are provided
+if [[ -z "$1" ]]; then
+  echo "Usage: $0 <organization>"
+  echo "Example: $0 bcgov-c"
+  exit 1
+fi
+
+ORG="$1"
+if [[ ! "$ORG" =~ ^(bcgov|bcgov-nr|bcgov-c)$ ]]; then
+  echo "Error: Organization must be one of [bcgov, bcgov-nr, bcgov-c]"
+  exit 1
+fi
+
 ANNOTATION_KEY="composer.io.nrs.gov.bc.ca/generators"
 
 # Check if already logged in

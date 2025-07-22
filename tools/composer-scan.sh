80c4eee368226cdb8bacf41324c6051331efd209
van der Hooft, David WLRS:EX <david.vanderhooft@gov.bc.ca>
2025-07-22 11:41:44 -0700

    feat: use gh jq

--- tools/composer-scan.sh
+++ tools/composer-scan.sh
@@ -28,7 +28,7 @@ fi
 TOKEN=$(gh auth token)
 
 # Get all repositories in the organization
-REPOS=$(gh api orgs/$ORG/repos --paginate | jq --raw-output 'map(select(.archived == false)) | .[].name')
+REPOS=$(gh api orgs/$ORG/repos --paginate --jq 'map(select(.archived == false)) | .[].name')
 
 echo "Scanning repositories for catalog-info.yaml files with the annotation $ANNOTATION_KEY..."
 

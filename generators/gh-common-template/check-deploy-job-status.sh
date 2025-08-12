7f05de178b7296f1ed23e097cdcb3aca08d777aa
GraceRuan <grace.ruan@gov.bc.ca>
2025-08-12 14:22:51 -0700

    feat: update trigger url in deploy.yaml and intention files

--- generators/gh-common-template/check-deploy-job-status.sh
+++ generators/gh-common-template/check-deploy-job-status.sh
@@ -7,12 +7,14 @@ echo "Current GitHub task start: $GH_TASK_START"
 sleep 30
 MAX_WAIT=30
 
-EVENT_REASON=$(echo -n "Github action: ${GITHUB_RUN_ID}" | jq -sRr @uri)
+TRIGGER_ID=$(echo -n "${SERVICE_NAME} ${GITHUB_RUN_ID}" | jq -sRr @uri)
+
+QUERY_URL="${BROKER_URL}/v1/intention/search?where=%7B%22actions.action%22%3A%22${ACTION_NAME}%22%2C%22event.trigger.id%22%3A%22${TRIGGER_ID}%22%2C%22actions.service.project%22%3A%22${SERVICE_PROJECT}%22%2C%22actions.service.name%22%3A%22${SERVICE_NAME}%22%2C%22actions.service.environment%22%3A%22${ENVIRONMENT}%22%2C%22event.provider%22%3A%22${PROVIDER_NAME}%22%7D&offset=0&limit=1"
 
 # Wait for the Jenkins deployment job to be triggered (max 90s)
 for ((i=1; i<=MAX_WAIT; i++)); do
   RESPONSE=$(curl -s -X 'POST' \
-    "${BROKER_URL}/v1/intention/search?where=%7B%22actions.action%22%3A%22${ACTION_NAME}%22%2C%22event.reason%22%3A%22${EVENT_REASON}%22%2C%22actions.service.project%22%3A%22${SERVICE_PROJECT}%22%2C%22actions.service.name%22%3A%22${SERVICE_NAME}%22%2C%22actions.service.environment%22%3A%22${ENVIRONMENT}%22%2C%22event.provider%22%3A%22${PROVIDER_NAME}%22%7D&offset=0&limit=1" \
+    "$QUERY_URL" \
     -H 'accept: application/json' \
     -H 'Authorization: Bearer '"${BROKER_JWT}"'' \
     -d '')
@@ -34,7 +36,7 @@ done
 # Wait for the deployment job to be closed (completed)
 for ((i=1; i<=MAX_WAIT; i++)); do
   RESPONSE=$(curl -s -X 'POST' \
-    "${BROKER_URL}/v1/intention/search?where=%7B%22actions.action%22%3A%22${ACTION_NAME}%22%2C%22event.reason%22%3A%22${EVENT_REASON}%22%2C%22actions.service.project%22%3A%22${SERVICE_PROJECT}%22%2C%22actions.service.name%22%3A%22${SERVICE_NAME}%22%2C%22actions.service.environment%22%3A%22${ENVIRONMENT}%22%2C%22event.provider%22%3A%22${PROVIDER_NAME}%22%7D&offset=0&limit=1" \
+    "$QUERY_URL" \
     -H 'accept: application/json' \
     -H 'Authorization: Bearer '"${BROKER_JWT}"'' \
     -d '')
